a3698d389e8fe3585d200b4658907c8e910bcd1c
xliang01 <xliang01@gmail.com>
2016-01-24 00:46:03 -0700

    Staircase both implementations are n^2

---
+++ PlusMinus/Staircase/Staircase/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Staircase
+//
+//  Created by Xiao Liang on 1/23/16.
+//  Copyright © 2016 MindMountain. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
